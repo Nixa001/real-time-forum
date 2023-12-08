33c4ec30ff75efd9e2fa38ffa91104452802279e
nifaye <nicolasfaye31@gmail.com>
2023-12-08 19:15:40 +0000

    Add post

--- views/scripts/home/main.js
+++ views/scripts/home/main.js
@@ -5,54 +5,61 @@ export function renderMainBody() {
 
     const sidebarRight = renderSidebarRight()
     const mainBody = `
-        <main class="main_body">
-            <div class="main">
-                <div class="post_div">
-                    <div class="header_post">
-                        <div class="info_post">
-                            <img src="/views/assets/profil.jpg" alt="profil" srcset="">
-                            <h3 class="user_name_post">Nicolas Faye</h3>
-                            <span class="username_post">@nixa001</span>
-                        </div>
-                        <h4 class="title_post">Lorem ipsum dolor sit, amet consectetur adipisic ingamet consectetur
-                            adipisicing elit.
-                            Voluptate, aspernatur. Dolorum, impedit. Modi, beataes</h4>
-                    </div>
-                    <div class="img_post"><img src="/views/assets/imagepost2.jpg" alt="" srcset=""></div>
-                    <div class="footer_post">
-                        <div class="like_post"><img src="/views/assets/icons/likew.png" alt="" srcset=""><span>320</span>
-                        </div>
-                        <div class="dislike_post"><img src="/views/assets/icons/dislikew.png" alt=""
-                                srcset=""><span>17</span>
-                        </div>
-                        <div class="comment_post"><img src="/views/assets/icons/comment.png" alt=""
-                                srcset=""><span>110</span></div>
-                    </div>
-                </div>
-                <div class="post_div">
-                    <div class="header_post">
-                        <div class="info_post">
-                            <img src="/views/assets/profilibg.jpg" alt="profil" srcset="">
-                            <h3 class="user_name_post">Ibrahima Gueye</h3>
-                            <span class="username_post">@ibg</span>
-                        </div>
-                        <h4 class="title_post">Lorem ipsum dolor sit, amet consectetur adipisic ingamet consectetur
-                            adipisicing elit.
-                            Voluptate, aspernatur. Dolorum, impedit. Modi, beataes</h4>
-                    </div>
-                    <div class="img_post"><img src="/views/assets/imagepost.jpg" alt="" srcset=""></div>
-                    <div class="footer_post">
-                        <div class="like_post"><img src="/views/assets/icons/likew.png" alt="" srcset=""><span>320</span>
-                        </div>
-                        <div class="dislike_post"><img src="/views/assets/icons/dislikew.png" alt=""
-                                srcset=""><span>17</span>
-                        </div>
-                        <div class="comment_post"><img src="/views/assets/icons/comment.png" alt=""
-                                srcset=""><span>110</span></div>
-                    </div>
-                </div>
+    <main class="main_body">
+      <div class="main">
+        <div class="create_post">
+          <form class="form_post" action="/post">
+            <textarea class="title_post" placeholder="What's Happening ?"></textarea>
+            <div class="send_post">
+              <label for="image_post" class="upload_image material-symbols-outlined">image</label>
+              <input type="file" name="image_post" id="image_post" hidden>
+              <input class="submit_post material-icons" type="submit" value="send">
             </div>
+          </form>
         </div>
+  
+        <div class="post_div">
+          <div class="header_post">
+            <div class="info_post">
+              <img src="/views/assets/profil.jpg" alt="profil" srcset="">
+              <h3 class="user_name_post">Nicolas Faye</h3>
+              <span class="username_post">@nixa001</span>
+            </div>
+            <h4 class="title_post">Lorem ipsum dolor sit, amet consectetur adipisic ingamet consectetur
+              adipisicing elit.
+              Voluptate, aspernatur. Dolorum, impedit. Modi, beataes</h4>
+          </div>
+          <div class="img_post"><img src="/views/assets/imagepost2.jpg" alt="" srcset=""></div>
+          <div class="footer_post">
+            <div class="like_post"><img src="/views/assets/icons/likew.png" alt="" srcset=""><span>320</span>
+            </div>
+            <div class="dislike_post"><img src="/views/assets/icons/dislikew.png" alt="" srcset=""><span>17</span>
+            </div>
+            <div class="comment_post"><img src="/views/assets/icons/comment.png" alt="" srcset=""><span>110</span></div>
+          </div>
+        </div>
+        <div class="post_div">
+          <div class="header_post">
+            <div class="info_post">
+              <img src="/views/assets/profilibg.jpg" alt="profil" srcset="">
+              <h3 class="user_name_post">Ibrahima Gueye</h3>
+              <span class="username_post">@ibg</span>
+            </div>
+            <h4 class="title_post">Lorem ipsum dolor sit, amet consectetur adipisic ingamet consectetur
+              adipisicing elit.
+              Voluptate, aspernatur. Dolorum, impedit. Modi, beataes</h4>
+          </div>
+          <div class="img_post"><img src="/views/assets/imagepost.jpg" alt="" srcset=""></div>
+          <div class="footer_post">
+            <div class="like_post"><img src="/views/assets/icons/likew.png" alt="" srcset=""><span>320</span>
+            </div>
+            <div class="dislike_post"><img src="/views/assets/icons/dislikew.png" alt="" srcset=""><span>17</span>
+            </div>
+            <div class="comment_post"><img src="/views/assets/icons/comment.png" alt="" srcset=""><span>110</span></div>
+          </div>
+        </div>
+      </div>
+      </div>
         ${sidebarRight}
     </main>
     `;
--- views/scripts/index.js
+++ views/scripts/index.js
@@ -1,6 +1,6 @@
 import { renderMainBody } from "./home/main.js";
-import { renderProfil } from "./home/profil.js";
 import { renderSidebarLeft } from "./home/sidebarLeft.js";
+import { renderProfil } from "./profil/profil.js";
 import { header } from "/views/scripts/header.js";
 import { login } from "/views/scripts/login.js";
 import { register } from "/views/scripts/register.js";
