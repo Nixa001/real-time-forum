360fef95aeca107f0718aef5248c85390cd48d25
nifaye <nicolasfaye31@gmail.com>
2023-12-11 11:37:57 +0000

    refractor message style

--- views/scripts/index.js
+++ views/scripts/index.js
@@ -1,5 +1,6 @@
 import { renderMainBody } from "./home/main.js";
 import { renderSidebarLeft } from "./home/sidebarLeft.js";
+import { getHtmlMessages } from "./message/message.js";
 import { header } from "/views/scripts/header.js";
 import { login } from "/views/scripts/login.js";
 import { register } from "/views/scripts/register.js";
@@ -10,7 +11,8 @@ document.addEventListener("DOMContentLoaded", () => {
     // login()
     // register()
     const headerHtml = header()
-    // const sidebarLeft = renderSidebarLeft()
-    // const mainBodyHtml = renderMainBody();
-    // document.body.innerHTML = headerHtml + sidebarLeft + mainBodyHtml;
+    const sidebarLeft = renderSidebarLeft()
+    const mainBodyHtml = renderMainBody();
+    const message = getHtmlMessages()
+    document.body.innerHTML = headerHtml + sidebarLeft + mainBodyHtml
 });
